7eb3ab14d3300b7d11568a112556315300997710
Khanh Gaker <khanhsevenam@gmail.com>
2024-09-01 19:05:17 +0800

    ver 1.0.1

--- app/src/main/java/com/example/toastlibrary/MainActivity.kt
+++ app/src/main/java/com/example/toastlibrary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.toastlibrary
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ToastUtils
+
+        ToastUtils.getInstance(this).showToast("SHOW ERROR")
     }
 }
